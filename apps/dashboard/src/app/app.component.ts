830b0d938245fbbeb02bfbe29c5fb1a31e27b18d
Kamille Norris <kamionorato@gmail.com>
2023-06-23 22:02:27 -0400

    Login application

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '@ng-mf/shared/data-access-user';
+import { distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'ng-mf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit() {
+    this.isLoggedIn$
+      .pipe(distinctUntilChanged())
+      .subscribe(async (loggedIn) => {
+        // Queue the navigation after initialNavigation blocking is completed
+        setTimeout(() => {
+          if (!loggedIn) {
+            this.router.navigateByUrl('login');
+          } else {
+            this.router.navigateByUrl('');
+          }
+        });
+      });
+  }
 }
