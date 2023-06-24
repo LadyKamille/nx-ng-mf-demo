5deae095abb7dacbdbf1025fd4cb95998d3f8b8c
Kamille Norris <kamionorato@gmail.com>
2023-06-24 14:07:20 -0400

    Add tailwind, remove demo code

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { loadRemoteModule } from '@nx/angular/mf';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
@@ -7,9 +6,5 @@ export const appRoutes: Route[] = [
     path: 'login',
     loadChildren: () =>
       loadRemoteModule('login', './Module').then((m) => m.RemoteEntryModule),
-  },
-  {
-    path: '',
-    component: NxWelcomeComponent,
   },
 ];
--- apps/login/src/app/remote-entry/entry.module.ts
+++ apps/login/src/app/remote-entry/entry.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { RemoteEntryComponent } from './entry.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { remoteRoutes } from './entry.routes';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [RemoteEntryComponent, NxWelcomeComponent],
+  declarations: [RemoteEntryComponent],
   imports: [CommonModule, FormsModule, RouterModule.forChild(remoteRoutes)],
   providers: [],
 })
